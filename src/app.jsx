7f65e9a5d375dd58e169eb602da65b04df41b7b3
andy-wanganqi <andy.wanganqi@gmail.com>
2023-05-03 12:41:13 +1200

    77. React-Router 101

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,63 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { 
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/index.scss'
 
-ReactDOM.render(<p>This</p>, document.getElementById('app'))
+const DashboardPage = () => (
+  <div>
+    Dashboard Page
+  </div>
+)
+
+const AddExpensePage = () => (
+  <div>
+    Add Expense Page
+  </div>
+)
+
+const EditExpensePage = () => (
+  <div>
+    Edit Expense Page
+  </div>
+)
+
+const HelpPage = () => (
+  <div>
+    Help Page
+  </div>
+)
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <DashboardPage></DashboardPage>,
+  },
+  {
+    path: '/create',
+    element: <AddExpensePage></AddExpensePage>
+  },
+  {
+    path: '/edit',
+    element: <EditExpensePage></EditExpensePage>
+  },
+  {
+    path: '/help',
+    element: <HelpPage></HelpPage>
+  }
+])
+
+const routes = (
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
+ReactDOM.render(routes, document.getElementById('app'))
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
