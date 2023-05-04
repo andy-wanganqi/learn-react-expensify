17ff5079a37b85f095705d9f751d611064748c55
andy-wanganqi <andy.wanganqi@gmail.com>
2023-05-04 15:59:17 +1200

    86. Dispatching Actions

--- src/lab/redux101.jsx
+++ src/lab/redux101.jsx
@@ -2,6 +2,41 @@ import { createStore } from 'redux'
 
 const store = createStore((state = {
   count: 0
-}) => state)
+}, action) => {
+  console.log('CALL createStore')
+
+  if (action.type === 'INCREMENT_N') {
+    return {
+      count: state.count + 1
+    }
+  } else if (action.type === 'DECREMENT_N') {
+    return {
+      count: state.count - 1
+    }
+  } else if (action.type === 'RESET') {
+    return {
+      count: 0
+    }
+  } else {
+    return state
+  }
+})
+
+console.log(store.getState())
+
+// Increment
+store.dispatch({
+  type: 'INCREMENT_N'
+})
+
+// Decrement
+store.dispatch({
+  type: 'DECREMENT_N'
+})
+
+// Reset
+store.dispatch({
+  type: 'RESET'
+})
 
 console.log(store.getState())
