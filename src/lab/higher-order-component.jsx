4c96955e6ddbfb7b681783ec411db1488c0f55fa
andy-wanganqi <andy.wanganqi@gmail.com>
2023-05-07 22:55:13 +1200

    100. The Higher Order Component

---
+++ src/lab/higher-order-component.jsx
@@ -0,0 +1,24 @@
+// Higher Order Component (HOC) - A component that renders another component
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Info = (props) => (
+  <div>
+    <h1>INFO</h1>
+    <p>This info is: {props.info}</p>
+  </div>
+)
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAdmin && <p>This is component wrapper</p>}
+      <WrappedComponent {...props}/>
+    </div>
+  )
+}
+
+const AdminInfo = withAdminWarning(Info)
+
+ReactDOM.render(<AdminInfo isAdmin={false} info="There are some details" />, document.getElementById("app"))
