ec71e77e20c37acc984106c30d1acd512ce65e12
andy-wanganqi <andy.wanganqi@gmail.com>
2023-05-16 11:44:18 +1200

    109. Redux Dev Tools

--- src/store/configure-store.jsx
+++ src/store/configure-store.jsx
@@ -7,9 +7,11 @@ export default () => {
     expenses: expensesReducer,
     filters: filtersReducer,
   });
+  /* eslint-disable no-underscore-dangle */
   const store = createStore(
     reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+  /* eslint-enable */
   return store;
 };
