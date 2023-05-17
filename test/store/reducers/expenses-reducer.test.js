cfcf3097f9fbe6467db1af32fe096fc25978d357
andy-wanganqi <andy.wanganqi@gmail.com>
2023-05-17 14:02:57 +1200

    117. Testing Expenses Reducer

---
+++ test/store/reducers/expenses-reducer.test.js
@@ -0,0 +1,63 @@
+import { v4 as uuid } from 'uuid';
+import { expensesReducer } from '../../../src/store/reducers/expenses-reducer.jsx';
+import expenses from '../../fixtures/expenses.js';
+
+test('Should setup default expenses', () => {
+  const state = expensesReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual([]);
+});
+
+test('Should add new expense to empty array', () => {
+  const expense = expenses[0];
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense,
+  }
+  const state = expensesReducer(undefined, action);
+  expect(state).toEqual([expense]);
+});
+
+test('Should add new expense to non-empty array', () => {
+  const expense = { id: uuid(), description: 'Water bill', amount: 9000, note: '123', createdAt: 1684005530000 };
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense,
+  }
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([...expenses, expense]);
+});
+
+test('Should edit expense', () => {
+  const expense = { ...expenses[2] };
+  expense.description += " 2";
+  expense.amount += 100;
+  expense.note += '123';
+  expense.createdAt += 10000;
+
+  const action = {
+    type: 'EDIT_EXPENSE',
+    expense,
+  }
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([ expenses[0], expenses[1], expense, expenses[3], expenses[4] ]);
+});
+
+test('Should remove expense which exists', () => {
+  const { id } = expenses[2];
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id,
+  }
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([ expenses[0], expenses[1], expenses[3], expenses[4] ]);
+});
+
+test('Should remove expense which does not exist', () => {
+  const id = uuid();
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id,
+  }
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
--- test/store/selectors/expenses-selector.test.js
+++ test/store/selectors/expenses-selector.test.js
@@ -1,14 +1,6 @@
-import { v4 as uuid } from 'uuid';
 import moment from 'moment';
 import { getVisibleExpenses } from '../../../src/store/selectors/expenses-selector.jsx';
-
-const expenses = [
-  { id: uuid(), description: 'Gum', note: '5', amount: 195, createdAt: 0 },
-  { id: uuid(), description: 'Rent', note: '1', amount: 109500, createdAt: 1000 },
-  { id: uuid(), description: 'Credit Card', note: '2', amount: 4500, createdAt: 2000 },
-  { id: uuid(), description: 'Power Bill', note: '3', amount: 30000, createdAt: 4000 },
-  { id: uuid(), description: 'Water Bill', note: '4', amount: 20000, createdAt: 3000 },
-];
+import expenses from '../../fixtures/expenses.js';
 
 test('Should filter by text', () => {
   const filters = {
