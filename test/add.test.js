8857f0ac2a1ab71a9ef5b634820fdde40d43f123
andy-wanganqi <andy.wanganqi@gmail.com>
2023-05-18 00:35:29 +1200

    Enzyme, but abandoned in react 18. So RTL

--- test/add.test.js
+++
@@ -1,9 +0,0 @@
-const add = (a, b) => a + b;
-
-test('should add two numbers', () => {
-  const result = add(10, 20);
-  const exp = 30;
-  expect(result).toBe(exp);
-
-  // throw new Error(`message`);
-});
--- test/components/layout/header.test.js
+++ test/components/layout/header.test.js
@@ -1,9 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Header from '../../../src/components/layout/header.jsx';
 
-test('Should render Header', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+test('Should render Header', async () => {
+  render(<Header />, { wrapper: BrowserRouter });
+  expect(screen.getByText('Expensify')).toBeInTheDocument();
+  expect(screen.getByText('Go home')).toBeInTheDocument();
+  expect(screen.getByText('Add Expense')).toBeInTheDocument();
+  expect(screen.getByText('Help')).toBeInTheDocument();
 });
