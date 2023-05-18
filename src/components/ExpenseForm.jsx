cb7c25a77ea5f7171abc7e7a5bd11704a70a504f
andy-wanganqi <andy.wanganqi@gmail.com>
2023-05-18 13:55:20 +1200

    Introduce useState

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,102 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import DatePicker from "react-datepicker";
 
-class ExpenseForm extends React.Component {
-  constructor(props){
-    super(props);
-    const expense = props.expense;
-    this.state = {
-      id: expense ? expense.id : '',
-      description: expense ? expense.description : '',
-      amountText: expense ? (parseFloat(expense.amount) / 100).toString() : '',
-      note: expense ? expense.note : '',
-      createdAt: expense ? expense.createdAt : moment().startOf('day').valueOf(),
-      errorMessage: '',
-    };
-  }
-  render() {
-    return (
-      <div>
-        <form>
-          <input type="text" placeholder="Description" autoFocus 
-            value={this.state.description}
-            onChange={this.handleDescriptionChange}
-          />
-          <input type="text" placeholder="Amount" step="100" 
-            value={this.state.amountText}
-            onChange={this.handleAmountChange}
-          />
-          <textarea placeholder="Note (optional)"
-            value={this.state.note}
-            onChange={this.handleNoteChange}
-          ></textarea>
-          <DatePicker showIcon selected={moment(this.state.createdAt).toDate()} onChange={(date) => this.handleDateChange(date)} dateFormat="dd/MM/yyyy"/>
-          <button onClick={this.handleSaveExpense}>Save Expense</button>
-          {this.state.id && <button onClick={this.handleRemoveExpense}>Remove Expense</button>}
-        </form>
-        <div>
-          {this.state.errorMessage && <p>{this.state.errorMessage}</p>}
-        </div>
-      </div>
-    );
-  }
-  handleDescriptionChange = (e) => {
+const ExpenseForm = (props) => {
+  const { expense } = props;
+  const initExpense = {
+    id: expense ? expense.id : undefined,
+    description: expense ? expense.description : '',
+    amountText: expense ? (parseFloat(expense.amount) / 100).toString() : '',
+    note: expense ? expense.note : '',
+    createdAt: expense ? expense.createdAt : moment().startOf('day').valueOf(),
+  };
+  const [expenseInForm, setExpense] = useState(initExpense);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleDescriptionChange = (e) => {
     const description = e.target.value;
-    this.setState(() => ({
+    setExpense(() => ({
+      ...expenseInForm,
       description
     }));
-  }
-  handleAmountChange = (e) => {
+  };
+  const handleAmountChange = (e) => {
     const amountText = e.target.value;
     if (!amountText || amountText.match(/^\d{1,10}(\.\d{0,2})?$/)) {
-      this.setState(() => ({
+      setExpense(() => ({
+        ...expenseInForm,
         amountText
       }));
     }
-  }
-  handleNoteChange = (e) => {
+  };
+  const handleNoteChange = (e) => {
     const note = e.target.value;
-    this.setState(() => ({
+    setExpense(() => ({
+      ...expenseInForm,
       note
     }));
-  }
-  handleDateChange = (date) => {
+  };
+  const handleDateChange = (date) => {
     if(date) {
-      this.setState(() => ({
+      setExpense(() => ({
+        ...expenseInForm,
         createdAt: moment(date).valueOf()
       }));
     }
-  }
-  validateForm = () => {
-    if (!this.state.description || !this.state.amountText) {
-      this.setState(() => ({
-        errorMessage: 'Please provide description and amount.'
-      }));
+  };
+  const validateForm = () => {
+    if (!expenseInForm.description || !expenseInForm.amountText) {
+      setErrorMessage('Please provide description and amount.');
       return false;
     } else {
-      this.setState(() => ({
-        errorMessage: ''
-      }));
+      setErrorMessage('');
       return true;
     }
-  }
-  handleSaveExpense = (e) => {
+  };
+  const handleSaveExpense = (e) => {
     e.preventDefault();
-    if (!this.validateForm()){
+    if (!validateForm()){
       return;
     }
-    this.props.handleSaveExpense({
-      description: this.state.description,
-      amount: parseFloat(this.state.amountText) * 100,
-      note: this.state.note,
-      createdAt: moment(this.state.createdAt).valueOf(),
+    props.handleSaveExpense({
+      description: expenseInForm.description,
+      amount: parseFloat(expenseInForm.amountText) * 100,
+      note: expenseInForm.note,
+      createdAt: moment(expenseInForm.createdAt).valueOf(),
     });
-  }
-  handleRemoveExpense = (e) => {
+  };
+  const handleRemoveExpense = (e) => {
     e.preventDefault();
-    this.props.handleRemoveExpense();
-  }
-}
+    props.handleRemoveExpense();
+  };
+
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Description" autoFocus 
+          value={expenseInForm.description}
+          onChange={handleDescriptionChange}
+        />
+        <input type="text" placeholder="Amount" step="100" 
+          value={expenseInForm.amountText}
+          onChange={handleAmountChange}
+        />
+        <textarea placeholder="Note (optional)"
+          value={expenseInForm.note}
+          onChange={handleNoteChange}
+        ></textarea>
+        <DatePicker showIcon selected={moment(expenseInForm.createdAt).toDate()} onChange={(date) => handleDateChange(date)} dateFormat="dd/MM/yyyy"/>
+        <button onClick={handleSaveExpense}>Save Expense</button>
+        {expenseInForm.id && <button onClick={handleRemoveExpense}>Remove Expense</button>}
+      </form>
+      <div>
+        {errorMessage && <p>{errorMessage}</p>}
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseForm;
